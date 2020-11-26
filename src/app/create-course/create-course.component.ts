42e8e5698851ca1dbf3dabe7cf612288825c7f59
JHades <jhades.dev@gmail.com>
2020-11-26 17:05:15 +0100

    Angular Forms In Depth Course

--- src/app/create-course/create-course.component.ts
+++ src/app/create-course/create-course.component.ts
@@ -7,33 +7,12 @@ import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
 @Component({
   selector: 'create-course',
   templateUrl: './create-course.component.html',
-  styleUrls: ['./create-course.component.scss'],
-  providers: [{
-    provide: STEPPER_GLOBAL_OPTIONS, useValue: {showError: true}
-  }]
+  styleUrls: ['./create-course.component.scss']
 })
 export class CreateCourseComponent implements OnInit {
 
 
-  constructor() {
-
-  }
-
-
   ngOnInit() {
-
-
-
-  }
-
-
-  submit(step1, step2,step3) {
-
-    console.log("Step 1 form value", step1);
-
-    console.log("Step 2 form value", step2);
-
-    console.log("Step 3 form value", step3);
 
   }
 
