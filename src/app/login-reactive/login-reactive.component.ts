03e5dbe0488a267a45ce05a187b3e09f848f32ce
Your Name <you@example.com>
2020-11-27 16:18:57 +0100

    Angular Forms In Depth

--- src/app/login-reactive/login-reactive.component.ts
+++ src/app/login-reactive/login-reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 
 @Component({
@@ -8,6 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginReactiveComponent implements OnInit {
 
+   form = new FormGroup({
+       email: new FormControl('', {validators: [Validators.required, Validators.email]}),
+       password: new FormControl('', {validators: [Validators.required, Validators.minLength(8)]})
+   });
 
   constructor() {
 
