2ee68bcc31886c00916756c1ca362f673ba6a3e3
Your Name <you@example.com>
2021-01-22 12:29:43 +0100

    Angular Forms In Depth

--- src/app/address-form/address-form.component.ts
+++ src/app/address-form/address-form.component.ts
@@ -13,12 +13,23 @@ import {noop, Subscription} from 'rxjs';
 @Component({
   selector: 'address-form',
   templateUrl: './address-form.component.html',
-  styleUrls: ['./address-form.component.scss']
+  styleUrls: ['./address-form.component.scss'],
+  providers: [
+      {
+          provide: NG_VALUE_ACCESSOR,
+          multi:true,
+          useExisting: AddressFormComponent
+      }
+  ]
 })
-export class AddressFormComponent {
+export class AddressFormComponent implements ControlValueAccessor, OnDestroy {
 
     @Input()
     legend:string;
+
+    onTouched = () => {};
+
+    onChangeSub: Subscription;
 
     form: FormGroup = this.fb.group({
         addressLine1: [null, [Validators.required]],
@@ -28,9 +39,51 @@ export class AddressFormComponent {
     });
 
     constructor(private fb: FormBuilder) {
+
+    }
+
+    registerOnChange(onChange: any) {
+        this.onChangeSub = this.form.valueChanges.subscribe(onChange);
+    }
+
+    ngOnDestroy() {
+        this.onChangeSub.unsubscribe();
+    }
+
+
+    writeValue(value: any) {
+        if (value) {
+            this.form.setValue(value);
+        }
+    }
+
+    registerOnTouched(onTouched: any) {
+        this.onTouched = onTouched;
+    }
+
+    setDisabledState(disabled:boolean) {
+        if (disabled) {
+            this.form.disable();
+        }
+        else {
+            this.form.enable();
+        }
+
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
