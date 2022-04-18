906a2de25d92dd11244f92915d80dc2c5995d60a
Apurva Yadav <Apurva-KIT@users.noreply.github.com>
2022-04-18 15:58:23 +0530

    Compilation issue resolved

--- server/search-lessons.route.ts
+++ server/search-lessons.route.ts
@@ -1,39 +1,55 @@
-
-
-
-import {Request, Response} from 'express';
-import {LESSONS} from './db-data';
-import {setTimeout} from 'timers';
-
-
+import { Request, Response } from "express";
+import { COURSES, LESSONS } from "./db-data";
+import { setTimeout } from "timers";
 
 export function searchLessons(req: Request, res: Response) {
-
   const queryParams = req.query;
 
-    const courseId = queryParams.courseId,
-          filter = queryParams.filter || '',
-          sortOrder = queryParams.sortOrder || 'asc',
-          pageNumber = parseInt(queryParams.pageNumber) || 0,
-          pageSize = parseInt(queryParams.pageSize) || 3;
+  const courseUrl = queryParams.courseUrl,
+    filter = queryParams.filter || "",
+    sortOrder = queryParams.sortOrder || "asc",
+    pageNumber = parseInt(queryParams.pageNumber as string, 10) || 0,
+    pageSize = parseInt(queryParams.pageSize as string, 10) || 3;
 
-    let lessons = Object.values(LESSONS).filter(lesson => lesson.courseId === courseId).sort((l1, l2) => l1.id - l2.id);
+  const courses: any = Object.values(COURSES);
 
-    if (filter) {
-       lessons = lessons.filter(lesson => lesson.description.trim().toLowerCase().search(filter.toLowerCase()) >= 0);
-    }
+  const course = courses.find((course) => course.url == courseUrl);
 
-    if (sortOrder === 'desc') {
-        lessons = lessons.reverse();
-    }
+  let lessons;
 
-    const initialPos = pageNumber * pageSize;
+  if (courseUrl) {
+    lessons = Object.values(LESSONS)
+      .filter((lesson) => lesson.courseId == course.id)
+      .sort((l1, l2) => l1.id - l2.id);
+  } else {
+    lessons = Object.values(LESSONS);
+  }
 
-    const lessonsPage = lessons.slice(initialPos, initialPos + pageSize);
+  if (filter) {
+    lessons = lessons.filter(
+      (lesson) =>
+        lesson.description
+          .trim()
+          .toLowerCase()
+          .search(filter[0].toLowerCase()) >= 0
+    );
+  }
 
-    setTimeout(() => {
-        res.status(200).json({payload: lessonsPage});
-    }, 1000);
+  if (sortOrder == "desc") {
+    lessons = lessons.reverse();
+  }
 
+  const initialPos = pageNumber * pageSize;
 
+  const lessonsPage = lessons
+    .slice(initialPos, initialPos + pageSize)
+    .map((lesson) => {
+      const newLesson = { ...lesson };
+      delete newLesson.videoId;
+      return newLesson;
+    });
+
+  setTimeout(() => {
+    res.status(200).json({ payload: lessonsPage });
+  }, 400);
 }
