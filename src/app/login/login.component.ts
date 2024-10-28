e46a39befe0e41c88fce3ecdb6c24d6ae98d5736
Gisela Lanciani <giselalanciani@gmail.com>
2024-10-28 09:59:06 -0300

    loginForm

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,21 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
+  constructor() {}
 
-
-  constructor() {
-
-
+  ngOnInit() {}
+  login(loginForm: NgForm) {
+    console.log(loginForm.value, loginForm.valid);
   }
-
-  ngOnInit() {
-
-  }
-
 }
