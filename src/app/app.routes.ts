8e85c934de12e32ca261edcfece638bd43975a78
vasco <jhades.dev@gmail.com>
2026-06-07 16:00:23 +0200

    prepare Angular Signals In Depth

---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CourseComponent } from './course/course.component';
+import { courseResolver } from './services/course.resolver';
+import { CreateCourseComponent } from './create-course/create-course.component';
+import { LoginComponent } from './login/login.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'courses/:id', component: CourseComponent, resolve: { course: courseResolver } },
+  { path: 'add-new-course', component: CreateCourseComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '/' }
+];
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { CoursesService } from './app/services/courses.service';
-import { provideHttpClient, withXhr, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { AppRoutingModule } from './app/app-routing.module';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
 if (environment.production) {
   enableProdMode();
@@ -12,8 +13,8 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(AppRoutingModule),
+    provideRouter(routes),
     CoursesService,
-    provideHttpClient(withXhr(), withInterceptorsFromDi())
+    provideHttpClient(withInterceptorsFromDi())
   ]
 }).catch(err => console.log(err));
