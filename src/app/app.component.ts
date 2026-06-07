6ca16c8194a2b49212a204ea5987b0883ba0b9be
vasco <jhades.dev@gmail.com>
2026-06-07 16:06:22 +0200

    prepare Angular Signals In Depth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   changeDetection: ChangeDetectionStrategy.Eager,
-  imports: [RouterLink, RouterOutlet]
+  imports: [RouterLink, RouterLinkActive, RouterOutlet]
 })
 export class AppComponent {
   navOpen = false;
