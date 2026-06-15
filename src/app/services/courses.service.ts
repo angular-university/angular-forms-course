c3c72a9431b990f784eae1605caa8fcbdb7d1160
vasco <jhades.dev@gmail.com>
2026-06-15 13:27:24 +0200

    prepare Angular Forms In Depth (Signals Edition)

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Course } from '../model/course';
 import { Lesson } from '../model/lesson';
@@ -9,32 +10,32 @@ export class CoursesService {
   private http = inject(HttpClient);
 
   findCourseById(courseId: number): Promise<Course> {
-    return this.http.get<Course>(`/api/courses/${courseId}`).toPromise() as Promise<Course>;
+    return lastValueFrom(this.http.get<Course>(`/api/courses/${courseId}`));
   }
 
   findCourseCategories(): Promise<any[]> {
-    return this.http.get<any>('/api/course-categories').pipe(
+    return lastValueFrom(this.http.get<any>('/api/course-categories').pipe(
       map(res => res['categories'])
-    ).toPromise() as Promise<any[]>;
+    ));
   }
 
   findAllCourses(): Promise<Course[]> {
-    return this.http.get<any>('/api/courses').pipe(
+    return lastValueFrom(this.http.get<any>('/api/courses').pipe(
       map(res => res['payload'] as Course[])
-    ).toPromise() as Promise<Course[]>;
+    ));
   }
 
   findLessons(
     courseId: number, filter = '', sortOrder = 'asc',
     pageNumber = 0, pageSize = 3
   ): Promise<Lesson[]> {
-    return this.http.get<any>('/api/lessons', {
+    return lastValueFrom(this.http.get<any>('/api/lessons', {
       params: new HttpParams()
         .set('courseId', courseId.toString())
         .set('filter', filter)
         .set('sortOrder', sortOrder)
         .set('pageNumber', pageNumber.toString())
         .set('pageSize', pageSize.toString())
-    }).pipe(map(res => res['payload'] as Lesson[])).toPromise() as Promise<Lesson[]>;
+    }).pipe(map(res => res['payload'] as Lesson[])));
   }
 }
