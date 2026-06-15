1011e0fceeaf9accce26cc7c7678631969323cbc
vasco <jhades.dev@gmail.com>
2026-06-15 16:18:23 +0200

    prepare Angular Forms In Depth (Signals Edition)

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,7 +1,6 @@
 import { Component, input, output, signal } from '@angular/core';
 import { Course } from '../model/course';
 import { form, FormField, FormRoot, required } from '@angular/forms/signals';
-import { FieldErrorComponent } from '../field-error/field-error.component';
 
 type CourseFormData = {
   description: string;
@@ -14,7 +13,7 @@ type CourseFormData = {
   selector: 'course-dialog',
   templateUrl: './course-dialog.component.html',
   styleUrls: ['./course-dialog.component.css'],
-  imports: [FormField, FormRoot, FieldErrorComponent]
+  imports: [FormField, FormRoot]
 })
 export class CourseDialogComponent {
   course = input.required<Course>();
